fd1bff789a1fa9a22496022af94cc406f724b643
KerryCooper <maxqofficial@foxmail.com>
2022-08-15 09:34:00 +0800

    (22.08.15)

---
+++ test_run.py
@@ -0,0 +1,13 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+# 直接运行 方便调试
+
+from  demucs.separate import main
+
+if __name__ == '__main__':
+    main()
